d9068c33cee7052943f1de3be607d16063e14d8b
antonioroque200OK <antonioroque200OK@users.noreply.github.com>
2021-05-29 23:03:40 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.cfc5eb27b5f514c0a8b04736f24ec470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201bbd28cdee709de0d447248fbd40e6",
+    "url": "https://antonioroque200OK.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ea17323bca3171b613ae",
+    "url": "https://antonioroque200OK.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "f34c76f26f6299f67ce4",
+    "url": "https://antonioroque200OK.github.io/task-management-frontend/static/js/2.5080ab92.chunk.js"
+  },
+  {
+    "revision": "ea17323bca3171b613ae",
+    "url": "https://antonioroque200OK.github.io/task-management-frontend/static/js/main.8b2b00cc.chunk.js"
+  },
+  {
+    "revision": "f8f63091aba845f06f7c",
+    "url": "https://antonioroque200OK.github.io/task-management-frontend/static/js/runtime~main.4544afa3.js"
+  }
+]);
